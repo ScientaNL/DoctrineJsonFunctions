ebcffe27ccd5cd152c3d0ae16848dd1caec213e9
Krzysztof Śmiałek <admin@avensome.net>
2017-08-03 12:13:23 +0200

    Extract common code

--- src/Query/AST/Functions/Postgresql/JsonGetPathText.php
+++ src/Query/AST/Functions/Postgresql/JsonGetPathText.php
@@ -2,36 +2,9 @@
 
 namespace Syslogic\DoctrineJsonFunctions\Query\AST\Functions\Postgresql;
 
-use Doctrine\ORM\Query\Lexer;
-use Doctrine\ORM\Query\Parser;
-
 
 class JsonGetPathText extends PostgresqlBinaryFunctionNode
 {
 	const FUNCTION_NAME = 'JSON_GET_PATH_TEXT';
 	const OPERATOR = '#>>';
-
-	/** @var \Doctrine\ORM\Query\AST\Node */
-	public $jsonData;
-
-	/** @var \Doctrine\ORM\Query\AST\Node */
-	public $jsonPath;
-
-
-	/**
-	 * @param Parser $parser
-	 */
-	public function parse(Parser $parser)
-	{
-		$parser->match(Lexer::T_IDENTIFIER);
-		$parser->match(Lexer::T_OPEN_PARENTHESIS);
-
-		$this->jsonData = $parser->StringPrimary();
-
-		$parser->match(Lexer::T_COMMA);
-
-		$this->jsonPath = $parser->StringPrimary();
-
-		$parser->match(Lexer::T_CLOSE_PARENTHESIS);
-	}
 }
--- src/Query/AST/Functions/Postgresql/PostgresqlBinaryFunctionNode.php
+++ src/Query/AST/Functions/Postgresql/PostgresqlBinaryFunctionNode.php
@@ -5,6 +5,8 @@ namespace Syslogic\DoctrineJsonFunctions\Query\AST\Functions\Postgresql;
 
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 
 abstract class PostgresqlBinaryFunctionNode extends FunctionNode
@@ -14,6 +16,25 @@ abstract class PostgresqlBinaryFunctionNode extends FunctionNode
 
     /** @var \Doctrine\ORM\Query\AST\Node */
     protected $jsonPath;
+
+
+    /**
+     * @param Parser $parser
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->jsonData = $parser->StringPrimary();
+
+        $parser->match(Lexer::T_COMMA);
+
+        $this->jsonPath = $parser->StringPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
 
     /**
      * @param SqlWalker $sqlWalker
