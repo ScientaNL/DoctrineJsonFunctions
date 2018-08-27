29a03a4b921d078786cd954e236084cba719a1d3
Ali Alwash <alitrix@gmail.com>
2018-08-27 10:36:40 +0200

    #24 Like requested by Hikariii, don't extend JsonKeys since it can change in the future. Instead copy the class behaviour and overwrite the needed parts

--- src/Query/AST/Functions/Mariadb/JsonExists.php
+++ src/Query/AST/Functions/Mariadb/JsonExists.php
@@ -8,12 +8,63 @@ use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
-use Syslogic\DoctrineJsonFunctions\Query\AST\Functions\Mysql\JsonKeys;
 
 /**
  * "JSON_EXISTS" "(" StringPrimary "," StringPrimary  ")"
  */
-class JsonExists extends JsonKeys
+class JsonExists extends FunctionNode
 {
-	const FUNCTION_NAME = 'JSON_EXISTS';
+    const FUNCTION_NAME = 'JSON_EXISTS';
+
+    /**
+     * @var \Doctrine\ORM\Query\AST\Node
+     */
+    public $jsonDocExpr;
+
+    /**
+     * @var \Doctrine\ORM\Query\AST\Node
+     */
+    public $jsonPathExpr;
+
+    /**
+     * @param SqlWalker $sqlWalker
+     * @return string
+     * @throws DBALException
+     */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        $jsonDocExpr = $sqlWalker->walkStringPrimary($this->jsonDocExpr);
+
+        $jsonPath = '';
+        if (!empty($this->jsonPathExpr)) {
+            $jsonPath = ', ' . $sqlWalker->walkStringPrimary($this->jsonPathExpr);
+        }
+
+        if ($sqlWalker->getConnection()->getDatabasePlatform() instanceof MySqlPlatform)
+        {
+            return sprintf('%s(%s)', static::FUNCTION_NAME, $jsonDocExpr . $jsonPath);
+        }
+
+        throw DBALException::notSupported(static::FUNCTION_NAME);
+    }
+
+    /**
+     * @param Parser $parser
+     * @throws \Doctrine\ORM\Query\QueryException
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->jsonDocExpr = $parser->StringPrimary();
+
+        if ($parser->getLexer()->isNextToken(Lexer::T_COMMA)) {
+            $parser->match(Lexer::T_COMMA);
+            $this->jsonPathExpr = $parser->StringPrimary();
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
 }
