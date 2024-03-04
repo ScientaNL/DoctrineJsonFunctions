afcb6af4d1421f795fec5cbf29c02aaf486f21c7
Alexander M. Turek <me@derrabus.de>
2024-03-04 08:56:53 +0100

    Support ORM 3

--- tests/Entities/Blank.php
+++ tests/Entities/Blank.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Scienta\DoctrineJsonFunctions\Tests\Entities;
 
-/**
- * @Entity
- */
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+/** @Entity */
+#[Entity]
 class Blank
 {
     /** @Id @Column(type="string") @GeneratedValue */
+    #[Id, Column(type: Types::STRING), GeneratedValue]
     public $id;
 }
