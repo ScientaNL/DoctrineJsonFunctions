66ee47bb3380c5cc8c18f812081887a6ff724d97
Hikariii <s.derooij@scienta.nl>
2024-03-08 10:03:31 +0100

    Remove php-7.4/annotation support

--- tests/Entities/JsonData.php
+++ tests/Entities/JsonData.php
@@ -10,19 +10,15 @@ use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 
-/** @Entity */
 #[Entity]
 class JsonData
 {
-    /** @Id @Column(type="string") @GeneratedValue */
     #[Id, Column(type: Types::STRING), GeneratedValue]
     public $id;
 
-    /** @Column(type="json") */
     #[Column(type: Types::JSON)]
     public $jsonCol;
 
-    /** @Column(type="json") */
     #[Column(type: Types::JSON)]
     public $jsonData;
 }
