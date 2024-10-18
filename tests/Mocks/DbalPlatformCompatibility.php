20e0cfba9269b8f690931b28baabb51fe134d92c
S. de Rooij <sebastiaan.de.rooij@woodwing.com>
2024-10-18 10:15:05 +0200

    Add phpstan and fix some related issues
    Fix compatibility issues with older composer versions
    Run phpstan in ci

--- tests/Mocks/DbalPlatformCompatibility.php
+++ tests/Mocks/DbalPlatformCompatibility.php
@@ -5,32 +5,33 @@ declare(strict_types=1);
 namespace Scienta\DoctrineJsonFunctions\Tests\Mocks;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Driver;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Platforms\DateIntervalUnit;
 use Doctrine\DBAL\Platforms\Keywords\KeywordList;
 use Doctrine\DBAL\Schema\AbstractSchemaManager;
 use Doctrine\DBAL\Schema\TableDiff;
 use Doctrine\DBAL\TransactionIsolationLevel;
 use ReflectionMethod;
+use Scienta\DoctrineJsonFunctions\DBALCompatibility;
 
-if ((new ReflectionMethod(Driver::class, 'getDatabasePlatform'))->getNumberOfParameters() === 1) {
+if ((new ReflectionMethod(AbstractPlatform::class, 'getLocateExpression'))->isAbstract()) {
     // DBAL 4
     /** @internal */
     trait DbalPlatformCompatibility
     {
         public function getLocateExpression(string $string, string $substring, ?string $start = null): string
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         public function getDateDiffExpression(string $date1, string $date2): string
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         protected function getDateArithmeticIntervalExpression(string $date, string $operator, string $interval, DateIntervalUnit $unit): string
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         /**
@@ -38,42 +39,42 @@ if ((new ReflectionMethod(Driver::class, 'getDatabasePlatform'))->getNumberOfPar
          */
         public function getAlterTableSQL(TableDiff $diff): array
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         public function getListViewsSQL(string $database): string
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         public function getSetTransactionIsolationSQL(TransactionIsolationLevel $level): string
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         public function getDateTimeTypeDeclarationSQL(array $column): string
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         public function getDateTypeDeclarationSQL(array $column): string
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         public function getTimeTypeDeclarationSQL(array $column): string
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         protected function createReservedKeywordsList(): KeywordList
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
 
         public function createSchemaManager(Connection $connection): AbstractSchemaManager
         {
-            throw NotSupported::new(__METHOD__);
+            throw DBALCompatibility::notSupportedPlatformException(__METHOD__);
         }
     }
 } else {
